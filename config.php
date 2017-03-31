6904c0228a373b3c7305880a949ec2172a3fdfe4
Ambre Saulnier <ambresaulnier@live.fr>
2017-03-31 12:03:12 +0200

    PDO

--- config.php
+++ config.php
@@ -3,3 +3,9 @@ $host = "127.0.0.1";
 $user = "root";
 $pass = "";
 $database = "ecommerce";
+
+try {
+    $bdd = new PDO("mysql:host=$host;dbname=$database;charset=utf8", $user, $pass);
+} catch(Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
