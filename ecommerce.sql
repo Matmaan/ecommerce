550bdf3212f29784cefa3913bec42382b6cd0a9d
Ambre Saulnier <ambresaulnier@live.fr>
2017-03-31 13:55:46 +0200

    Modification champs et ajout produits

--- ecommerce.sql
+++ ecommerce.sql
@@ -3,11 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
-<<<<<<< HEAD
--- Généré le :  Ven 31 Mars 2017 à 12:21
-=======
--- Généré le :  Ven 31 Mars 2017 à 12:07
->>>>>>> origin/master
+-- Généré le :  Ven 31 Mars 2017 à 13:54
 -- Version du serveur :  10.1.19-MariaDB
 -- Version de PHP :  5.6.28
 
@@ -27,17 +23,48 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `category`
+--
+
+CREATE TABLE `category` (
+  `id_category` int(11) NOT NULL,
+  `category` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `category`
+--
+
+INSERT INTO `category` (`id_category`, `category`) VALUES
+(1, 'Cauchemar'),
+(2, 'Erotique'),
+(3, 'Ado'),
+(4, 'Basique');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `product`
 --
 
 CREATE TABLE `product` (
   `id_product` int(11) NOT NULL,
+  `id_category` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL,
   `price` int(11) NOT NULL,
   `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `product`
+--
+
+INSERT INTO `product` (`id_product`, `id_category`, `name`, `description`, `image`, `price`, `quantity`) VALUES
+(1, 1, 'Le grand méchant loup', 'BOUH ! Vous allez vous pisser dessus', '', 0, 0),
+(3, 2, 'Nuit en folie', 'Vous êtes accompagné(e) de la personne de votre choix et elle vous fiat bien ce que vous voulez...', 'https://static.pexels.com/photos/570/sunny-summer-catcher-feathers.jpg', 0, 0),
+(4, 3, 'Le crush', 'Vous êtes tout ce que vous avez rêver d''être à l''âge ingrat', 'https://www.pexels.com/photo/girls-on-white-red-jersey-playing-hand-game-163465/', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -49,11 +76,7 @@ CREATE TABLE `users` (
   `id_user` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` text NOT NULL,
-<<<<<<< HEAD
   `role` varchar(55) DEFAULT 'user',
-=======
-  `role` varchar(55) NOT NULL,
->>>>>>> origin/master
   `login` varchar(255) NOT NULL,
   `registered_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -63,10 +86,17 @@ CREATE TABLE `users` (
 --
 
 --
+-- Index pour la table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id_category`);
+
+--
 -- Index pour la table `product`
 --
 ALTER TABLE `product`
-  ADD PRIMARY KEY (`id_product`);
+  ADD PRIMARY KEY (`id_product`),
+  ADD KEY `id_category` (`id_category`);
 
 --
 -- Index pour la table `users`
@@ -79,15 +109,30 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT pour la table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
 -- AUTO_INCREMENT pour la table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
   MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `product`
+--
+ALTER TABLE `product`
+  ADD CONSTRAINT `product_ibfk_1` FOREIGN KEY (`id_category`) REFERENCES `category` (`id_category`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
