373b3208a96b3c3c53f3383f8264b8d9820c86c1
Mathieu Briatte <briattemathieu@yahoo.fr>
2017-03-31 12:09:58 +0200

    Ajout formulaire

--- pages/inscription.php
+++ pages/inscription.php
@@ -4,7 +4,13 @@
 
 <main>
 	<form>
-		<label for="" class=""></label>
-		<input type="text" name="" class="form-control">
+		<label for="login" class="">Pseudo :</label>
+		<input type="text" name="login" class="form-control">
+		<label for="mail" class="">E-mail :</label>
+		<input type="text" name="mail" class="form-control">
+		<label for="pswd" class="">Mot de passe :</label>
+		<input type="password" name="pswd" class="form-control">
+		<br>
+		<button type="submit" class="btn btn-default btn-primary">Inscription</button>
 	</form>
 </main>
