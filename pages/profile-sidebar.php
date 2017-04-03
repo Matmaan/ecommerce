cb3ef6ba13e177c40c722fe8064f108169b6aea9
ClemenceChoupette <clemenceducornet@gmail.com>
2017-04-03 14:43:17 +0200

    page de profil

---
+++ pages/profile-sidebar.php
@@ -0,0 +1,6 @@
+<div class="list-group">
+  <a href="?page=update" class="list-group-item">Mettre à jour le profil</a>
+  <a href="?page=36" class="list-group-item">Mes articles</a>
+  <a href="?page=37" class="list-group-item">Créer un article</a>
+  <a href="?page=39" class="list-group-item">Déconnexion</a>
+</div>
