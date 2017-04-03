1186bf6f00deed4924744577af00f570f500638c
Ambre Saulnier <ambresaulnier@live.fr>
2017-04-03 12:28:19 +0200

    mot de passe oublié
    avec jeton de réinitialisation

---
+++ reset_password.php
@@ -0,0 +1,20 @@
+<?php echo'
+<form action="reset.php" method="POST">
+Votre adresse email: <input type="text" name="email" size="20" /><br />
+Nouveau mot de passe: <input type="password" name="password" size="20" /><br />
+Confirmation du nouveau mot de passe: <input type="password" name="confirmpassword"/><br />
+<input type="hidden" name="q" value="';
+
+if (isset($_GET["q"])) {
+
+    echo $_GET["q"];
+
+}
+
+    echo '" /><input type="submit" name="ResetPasswordForm" value="Reset Password" />
+
+</form>';
+
+ 
+
+?>
