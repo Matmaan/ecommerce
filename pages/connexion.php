2feae5f0f64a293699d5cc6b23be7f3d5ce4643e
Mathieu Briatte <briattemathieu@yahoo.fr>
2017-04-03 12:21:25 +0200

    Add function toConnect

--- pages/connexion.php
+++ pages/connexion.php
@@ -1,7 +1,11 @@
-<form>
+<?php
+include_once 'function/fnc.ToConnect.php';
+?>
+<form method="POST">
 	<label for="pseudo">Pseudonyme :</label>
 	<input type="text" name="pseudo" class="form-control">
 	<label for="pswd">Mot de passe :</label>
-	<input type="text" name="pswd" class="form-control">
-	<button type="submit">Connexion</button>
+	<input type="password" name="pswd" class="form-control">
+	<br>
+	<button type="submit" class="btn btn-default btn-primary">Inscription</button>
 </form>
