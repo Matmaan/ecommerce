33a4c27aa12dda77752b142410f791d75dba011f
ClemenceChoupette <clemenceducornet@gmail.com>
2017-04-03 15:38:58 +0200

    Contact profile

--- pages/contact-profile.php
+++ pages/contact-profile.php
@@ -3,8 +3,6 @@
                     <?php include_once 'profile-sidebar.php' ?>
                 </div>
                 <div class="col-md-9">
-
                     Bonjour <?php echo $_SESSION['user']->login; ?>
-
                 </div>
             </div>
