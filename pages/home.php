6c9d8061cdc5c5544d22c6fbc01865143041d630
PhilippeGruet <philippe.gruet@outlook.fr>
2017-04-03 12:19:29 +0200

    Filtre category

--- pages/home.php
+++ pages/home.php
@@ -20,15 +20,31 @@
             </form>
         </div>
     </div>
-    <span id="productList">
-        <?php foreach (getProducts(isset($_SESSION["nbProducts"]) ?$_SESSION["nbProducts"]: 12 ) as $article): ?>
-            <div class="col-md-3 well product" style="height: 300px">
-                <a href="?page=product&article=<?php echo $article->id_product; ?>">
-                    <img src="<?php echo $article->image?>" class="img-responsive img-thumbnail">
-                    <h3><?php echo $article->name; ?></h3>
-                </a>
-                <h4><?php echo $article->price; ?> €</h4>
-            </div>
-        <?php endforeach; ?>
-    </span>
+
+    <?php if(isset($_GET["category"])): ?>
+        <span id="productList">
+            <?php foreach (getProductsByCategory(isset($_SESSION["nbProducts"]) ?$_SESSION["nbProducts"]: 12,  ($_GET["category"])) as $article): ?>
+                <div class="col-md-3 well product" style="height: 300px">
+                    <a href="?page=product&article=<?php echo $article->id_product; ?>">
+                        <img src="<?php echo $article->image?>" class="img-responsive img-thumbnail">
+                        <h3><?php echo $article->name; ?></h3>
+                    </a>
+                    <h4><?php echo $article->price; ?> €</h4>
+                </div>
+            <?php endforeach; ?>
+        </span>
+    <?php else: ?>
+        <span id="productList">
+            <?php foreach (getProducts(isset($_SESSION["nbProducts"]) ?$_SESSION["nbProducts"]: 12 ) as $article): ?>
+                <div class="col-md-3 well product" style="height: 300px">
+                    <a href="?page=product&article=<?php echo $article->id_product; ?>">
+                        <img src="<?php echo $article->image?>" class="img-responsive img-thumbnail">
+                        <h3><?php echo $article->name; ?></h3>
+                    </a>
+                    <h4><?php echo $article->price; ?> €</h4>
+                </div>
+            <?php endforeach; ?>
+        </span>
+    <?php endif; ?>
+    
 </div>
